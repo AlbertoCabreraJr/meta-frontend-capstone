c4a2ceea38960fdbfd15eb6ff21a0e36962475a3
Alberto A. Cabrera Jr <alberto@Alberto-Mac.local>
2023-02-07 20:46:59 +0800

    For week 1

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <img src={logo} />
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a>Menu</a>
+        </li>
+        <li>
+          <a>Reservations</a>
+        </li>
+        <li>
+          <a>Order Online</a>
+        </li>
+        <li>
+          <a>Login</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
